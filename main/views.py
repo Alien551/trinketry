d6151a7cc438be0bd98bdab5bc8048a5d99191e4
Alien551 <xraydron@gmail.com>
2026-02-02 18:57:05 +0300

    add shop, about, faq, blog and gallery views

--- main/views.py
+++ main/views.py
@@ -3,4 +3,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Main index page.")
+    return HttpResponse("Главная страница")
+
+def shop(request):
+    return HttpResponse("Страница магазина")
+
+def about(request):
+    return HttpResponse("Страница обо мне")
+
+def faq(request):
+    return HttpResponse("Страница с ЧАВО")
+
+def blog(request):
+    return HttpResponse("Страница с блогом")
+
+def gallery(request):
+    return HttpResponse("Галерея работ")
