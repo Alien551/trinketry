fe36e74127b0200900575b3c14a45207867a2e91
Alien551 <xraydron@gmail.com>
2026-02-05 14:57:39 +0300

    small changes

--- config/urls.py
+++ config/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('gallery/', include('gallery.urls')),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/models.py
+++ shop/models.py
@@ -1,12 +1,9 @@
 from django.db import models
-from django.core.files.storage import FileSystemStorage
-
-fs = FileSystemStorage(location="../media/products")
 
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=50)
-    image = models.ImageField(storage=fs)
+    image = models.ImageField(upload_to='products/')
     cost = models.IntegerField()
     count = models.IntegerField(default=1)
 
