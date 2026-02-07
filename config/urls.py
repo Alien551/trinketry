eb9e00d60f2988ba17e2913bf25ba22e92714988
Alien551 <xraydron@gmail.com>
2026-02-07 23:41:04 +0300

    small qol edit

--- config/urls.py
+++ config/urls.py
@@ -27,5 +27,8 @@ urlpatterns = [
     path('account/', include("account.urls")),
     path('cart/', include("cart.urls")),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/admin.py
+++ shop/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Product
 # Register your models here.
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'cost', 'count', 'date')
+    list_display_links = ('id', 'name')
+
+admin.site.register(Product, ProductAdmin)
