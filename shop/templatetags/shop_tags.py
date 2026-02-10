06f9782b7ebc80836112544cfed4cc7e1fa042e7
Alien551 <xraydron@gmail.com>
2026-02-10 11:23:07 +0300

    templatetags for the shop

---
+++ shop/templatetags/shop_tags.py
@@ -0,0 +1,17 @@
+from django import template
+from django.shortcuts import get_object_or_404
+from shop.models import Product
+from cart.models import UserCart
+
+register = template.Library()
+
+@register.simple_tag
+def get_all_products_list():
+    """Возвращает писок всех товаров."""
+    return Product.objects.all()
+
+@register.simple_tag(takes_context=True)
+def get_current_product(context):
+    """Возращает один продукт по его ключу в таблице"""
+    pk = context["pk"]
+    return get_object_or_404(Product, pk=pk)
