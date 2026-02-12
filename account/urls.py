dff6f6109417e543013925c1fe084651dfd4bd0c
Alien551 <xraydron@gmail.com>
2026-02-13 02:10:27 +0300

    refactor account app

--- account/urls.py
+++ account/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = "account"
 urlpatterns = [
-    path("", views.index, name="account"), #страница аккаунта
-    path("login", views.login, name="login"), #вход в аккаунт
-    path("register", views.register, name="register"), #создание аккаунта
-    path("logout", views.logout, name="logout"),
+    path("", views.index, name="index"), #страница аккаунта
+    path("login/", views.login, name="login"), #вход в аккаунт
+    path("register/", views.register, name="register"), #создание аккаунта
+    path("logout/", views.logout, name="logout"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,28 +1,33 @@
+from math import log
 from django.shortcuts import render, redirect
-from django.contrib.auth import login as logIN, authenticate, logout as logOUT
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, RegisterForm
 
 # Create your views here.
+@login_required
 def index(request):
     if request.user.is_authenticated:
         context = {"title":"Аккаунт"}
         return render(request, 'account/index.html', context)
-    else:
-        return redirect('login')
 
 
 def login(request):
     form = LoginForm(data=request.POST or None)
     if request.user.is_authenticated:
-        return redirect('home')
+        return redirect('main:index')
     if request.method == 'POST':
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            user = authenticate(username=username, password=password)
+            user = auth.authenticate(username=username, password=password)
             if user is not None:
-                logIN(request, user)
-                return redirect('home')
+                auth.login(request, user)
+
+                if request.POST.get("next", None):
+                    return redirect(request.POST.get("next", None))
+                return redirect('main:index')
+
     return render(request, 'account/login.html', {'form':form})
 
 
@@ -31,13 +36,14 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            logIN(request, user)
-            return redirect('home')
+            auth.login(request, user)
+            return redirect('main:index')
     else:
         form = RegisterForm()
     return render(request, 'account/register.html', {'form':form})
 
 
+@login_required
 def logout(request):
-    logOUT(request)
-    return redirect('home')
+    auth.logout(request)
+    return redirect('main:index')
