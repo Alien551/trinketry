46c3cc3aa4b5a3090886d0e8a90f1b6fd9853c6a
Alien551 <xraydron@gmail.com>
2026-02-13 02:12:27 +0300

    some litle changes

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from django.conf.global_settings import LOGIN_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
+
+
+LOGIN_URL = "/account/login/"
--- gallery/urls.py
+++ gallery/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = "gallery"
 urlpatterns = [
-    path("", views.index, name="gallery"),
+    path("", views.index, name="index"),
 ]
